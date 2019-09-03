10d865f588d5eac1f52098adaaa52c06f6fac7c7
Miguel Partida <miguelpartida080497@gmail.com>
2019-09-03 11:21:01 -0500

    log out

--- app/src/main/java/com/miguel/android/trackyourexpenses/ui/activity/AccountActivity.kt
+++ app/src/main/java/com/miguel/android/trackyourexpenses/ui/activity/AccountActivity.kt
@@ -1,21 +1,54 @@
 package com.miguel.android.trackyourexpenses.ui.activity
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.miguel.android.trackyourexpenses.R
+import com.miguel.android.trackyourexpenses.common.PREF_KEEP_LOGIN
+import com.miguel.android.trackyourexpenses.common.PREF_TOKEN
+import com.miguel.android.trackyourexpenses.common.SharedPreferencesManager
 import com.miguel.android.trackyourexpenses.data.database.entity.Accounts
 import com.miguel.android.trackyourexpenses.ui.*
 import com.miguel.android.trackyourexpenses.ui.fragments.DashboardFragment
 import com.miguel.android.trackyourexpenses.ui.fragments.DashboardFragmentDirections
+import kotlinx.android.synthetic.main.navhost_account.*
 
 class AccountActivity: AppCompatActivity(), DashboardFragment.Callbacks,
 DashboardFragment.OnDeleteAccountListener{
 
+    lateinit var navigationController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.navhost_account)
+        navigationController = findNavController(R.id.myNavHostFragmentAccount)
+        navigationController.setGraph(R.navigation.navigation)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when(item?.itemId){
+            R.id.log_out ->{
+                navigationController.navigate(R.id.mainActivity)
+                SharedPreferencesManager.setSomeBoolValue(PREF_KEEP_LOGIN, false)
+                SharedPreferencesManager.setSomeStringValue(PREF_TOKEN, "")
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 
--- app/src/main/java/com/miguel/android/trackyourexpenses/ui/activity/DetailsActivity.kt
+++ app/src/main/java/com/miguel/android/trackyourexpenses/ui/activity/DetailsActivity.kt
@@ -1,24 +1,55 @@
 package com.miguel.android.trackyourexpenses.ui.activity
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.miguel.android.trackyourexpenses.R
+import com.miguel.android.trackyourexpenses.common.PREF_KEEP_LOGIN
+import com.miguel.android.trackyourexpenses.common.PREF_TOKEN
+import com.miguel.android.trackyourexpenses.common.SharedPreferencesManager
 import com.miguel.android.trackyourexpenses.data.ItemMov
 import com.miguel.android.trackyourexpenses.ui.fragments.NewMovementDialog
 import com.miguel.android.trackyourexpenses.ui.fragments.NewMovementFragment
 import com.miguel.android.trackyourexpenses.ui.OnMovementSelected
 import com.miguel.android.trackyourexpenses.ui.fragments.AccountDetailsFragmentDirections
+import kotlinx.android.synthetic.main.navhost_details.*
 
 class DetailsActivity: AppCompatActivity(), NewMovementDialog.OnItemListListener,
     OnMovementSelected {
+
+    lateinit var navigationController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.navhost_details)
-        findNavController(R.id.myNavHostFragmentDetails).setGraph(R.navigation.navigation_details, intent.extras)
+        navigationController = findNavController(R.id.myNavHostFragmentDetails)
+        navigationController.setGraph(R.navigation.navigation_details, intent.extras)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when(item?.itemId){
+            R.id.log_out ->{
+                navigationController.navigate(R.id.mainActivityDetails)
+                SharedPreferencesManager.setSomeBoolValue(PREF_KEEP_LOGIN, false)
+                SharedPreferencesManager.setSomeStringValue(PREF_TOKEN, "")
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onIncomeIdSelected(movementId: String?, view: View?) {
